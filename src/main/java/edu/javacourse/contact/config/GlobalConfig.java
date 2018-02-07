1613d8cf3b6b5f96e4547b29c10ba949ba6c15a0
Evgeniy Golubtsov <e.golubtsoff@gmail.com>
2018-02-07 11:13:37 +0700

    ver.1.3 - added readme.md - added launch's key

--- src/main/java/edu/javacourse/contact/config/GlobalConfig.java
+++ src/main/java/edu/javacourse/contact/config/GlobalConfig.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
-public class GlobalConfig
-{
+public class GlobalConfig {
     private static final String CONFIG_NAME = "contacts.properties";
+    private static final String LOCALE_EN = "en";
+    private static final String LOCALE_RU = "ru";
     private static final Properties GLOBAL_COFIG = new Properties();
 
     // Сделать начальную загрузку параметров из файла по умолчанию
@@ -14,13 +15,19 @@ public class GlobalConfig
         initGlobalConfig(null);
     }
 
-    // Сделать загрузку данных из конфигурационного файла, имя которого передано в виде параметра
-    // Если имя null или пустое - берем файл по умолчанию.
-    public static void initGlobalConfig(String name) throws IOException {
-        if (name != null && !name.trim().isEmpty()) {
-            GLOBAL_COFIG.load(GlobalConfig.class.getClassLoader().getResourceAsStream(name));
-        } else {
+    // Если язык выбран, то запускается приложение с хранением контактов в БД
+    // в противном случае запускается демо-режим с хранением в памяти;
+    // после перезапуска приложения все данные пропадают
+    public static void initGlobalConfig(String locale) throws IOException {
+        if (locale != null && !locale.trim().isEmpty() &&
+             (locale.equalsIgnoreCase("-" + LOCALE_EN) || locale.equalsIgnoreCase("-" + LOCALE_RU))) {
             GLOBAL_COFIG.load(GlobalConfig.class.getClassLoader().getResourceAsStream(CONFIG_NAME));
+            GLOBAL_COFIG.put("language", locale.replace("-",""));
+            GLOBAL_COFIG.put("dao.class", "edu.javacourse.contact.dao.ContactDbDAO");
+            createDB();
+        } else {
+            GLOBAL_COFIG.put("language", LOCALE_EN);
+            GLOBAL_COFIG.put("dao.class", "edu.javacourse.contact.dao.ContactSimpleDAO");
         }
     }
 
@@ -29,7 +36,7 @@ public class GlobalConfig
         return GLOBAL_COFIG.getProperty(property);
     }
 
-    public static void createDB(){
+    private static void createDB() {
         try {
             String drivers = System.getProperty("jdbc.drivers");
             if (drivers == null) {
@@ -37,14 +44,14 @@ public class GlobalConfig
             }
             String url = GLOBAL_COFIG.getProperty("db.url");
             String login = GLOBAL_COFIG.getProperty("db.login");
-            String password =GLOBAL_COFIG.getProperty("db.password");
+            String password = GLOBAL_COFIG.getProperty("db.password");
 
             try (Connection con = DriverManager.getConnection(url, login, password);
-                 Statement stmt = con.createStatement()){
+                 Statement stmt = con.createStatement()) {
                 DatabaseMetaData databaseMetadata = con.getMetaData();
                 ResultSet rs = databaseMetadata.getTables(null, null, "JC_CONTACT", null);
                 if (rs.next()) {
-                    stmt.addBatch("DROP TABLE JC_CONTACT");
+                    return;
                 }
                 stmt.addBatch("CREATE TABLE JC_CONTACT" +
                         "(" +
@@ -55,8 +62,6 @@ public class GlobalConfig
                         "EMAIL VARCHAR(50) NOT NULL," +
                         "PRIMARY KEY (CONTACT_ID)" +
                         ")");
-                stmt.addBatch("INSERT INTO JC_CONTACT (FIRST_NAME, LAST_NAME, PHONE, EMAIL) VALUES ('Peter','Belgy','+79112345678','peter@pisem.net')");
-                stmt.addBatch("INSERT INTO JC_CONTACT (FIRST_NAME, LAST_NAME, PHONE, EMAIL) VALUES ('Helga','Forte','+79118765432','helga@pisem.net')");
                 stmt.executeBatch();
             }
         } catch (Exception e) {
--- src/main/java/edu/javacourse/contact/test/ContactTest.java
+++ src/main/java/edu/javacourse/contact/test/ContactTest.java
@@ -11,8 +11,13 @@ public class ContactTest
 {
     public static void main(String[] args) {
         try {
-            GlobalConfig.initGlobalConfig();
-            GlobalConfig.createDB();
+            if (args.length == 0){
+                GlobalConfig.initGlobalConfig();
+            } else if (args.length == 1){
+                GlobalConfig.initGlobalConfig(args[0]);
+            } else {
+                return;
+            }
             GuiResource.initComponentResources();
         } catch (Exception ex) {
             ex.printStackTrace(System.out);
